071e7135c001160f97749f210ab0f33efb4cf1f4
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-02-22 19:50:06 +0200

    Add 1st version of the scraper
    Works for the Arctic Redpolls

---
+++ R/scraper.R
@@ -0,0 +1,18 @@
+library("magrittr")
+library("rvest")
+
+tarp_site <- html("https://www.justgiving.com/COTF2015AR/") 
+
+perc_raised <- tarp_site %>% 
+  html_nodes(xpath="//div//strong//em") %>%
+  extract2(1) %>%
+  html_text()
+
+sum_raised <- tarp_site %>%
+  html_nodes(xpath="//div//p//span[@class='raised-so-far']") %>%
+  html_text()
+
+n_donations <- tarp_site %>%
+  html_nodes(xpath="//div//p//span[@class='number-of-donations']") %>%
+  html_text() %>%
+  as.numeric()
