118f5170fa7ce065abd9cae188b328b5ea0a1c08
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-02-22 20:27:33 +0200

    Implement scraping multiple sites

--- R/scraper.R
+++ R/scraper.R
@@ -1,3 +1,4 @@
+library("dplyr")
 library("magrittr")
 library("rvest")
 
@@ -11,7 +12,14 @@ urls <- c("https://www.justgiving.com/COTF2015BLI/",
 
 scrape_data <- function(url) {
 
+  message("Scraping page ", url)
+  
   site <- html(url) 
+  
+  team_name <- site %>%
+    html_nodes(xpath="//div[@id='summary-top-content']//h2") %>%
+    html_text()
+  team_name <- gsub(" Fundraising Page", "", team_name)
   
   target <- site %>% 
     html_nodes(xpath="//div//p//em") %>%
@@ -19,13 +27,13 @@ scrape_data <- function(url) {
     html_text()
   target <- unlist(strsplit(target, "\\s+"))[4]
   
+  sum_raised <- site %>%
+    html_nodes(xpath="//div//p//span[@class='raised-so-far']") %>%
+    html_text()
+  
   perc_raised <- site %>% 
     html_nodes(xpath="//div//strong//em") %>%
     extract2(1) %>%
-    html_text()
-  
-  sum_raised <- site %>%
-    html_nodes(xpath="//div//p//span[@class='raised-so-far']") %>%
     html_text()
   
   n_donations <- site %>%
@@ -33,7 +41,12 @@ scrape_data <- function(url) {
     html_text() %>%
     as.numeric()
   
-  dat <- data.frame(perc_raised=perc_raised, )
+  dat <- data.frame(team_name=team_name, target=target, sum_raised=sum_raised, 
+                    perc_raised=perc_raised, n_donations=n_donations, 
+                    stringsAsFactors=FALSE)
   
-  return(target=target, )
+  return(dat)
 }
+
+donations_data <- lapply(urls, scrape_data)
+donations_data <- bind_rows(donations_data)
