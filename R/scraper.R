ebd359b7e8e579b30ad5afc3cb69142ef136f008
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-02-22 20:14:53 +0200

    Add 7 team urls

--- R/scraper.R
+++ R/scraper.R
@@ -1,24 +1,39 @@
 library("magrittr")
 library("rvest")
 
-tarp_site <- html("https://www.justgiving.com/COTF2015AR/") 
+urls <- c("https://www.justgiving.com/COTF2015BLI/",
+          "https://www.justgiving.com/COTF2015NGB/",
+          "https://www.justgiving.com/COTF-birding-ecotours/",
+          "https://www.justgiving.com/COTF2015BBR",
+          "https://www.justgiving.com/COTF2015AR",
+          "https://www.justgiving.com/COTF2015TNL/",
+          "https://www.justgiving.com/COTFRB")
 
-perc_raised <- tarp_site %>% 
-  html_nodes(xpath="//div//strong//em") %>%
-  extract2(1) %>%
-  html_text()
+scrape_data <- function(url) {
 
-sum_raised <- tarp_site %>%
-  html_nodes(xpath="//div//p//span[@class='raised-so-far']") %>%
-  html_text()
-
-target <- tarp_site %>% 
-  html_nodes(xpath="//div//p//em") %>%
-  extract2(1) %>%
-  html_text()
-target <- unlist(strsplit(target, "\\s+"))[4]
-
-n_donations <- tarp_site %>%
-  html_nodes(xpath="//div//p//span[@class='number-of-donations']") %>%
-  html_text() %>%
-  as.numeric()
+  site <- html(url) 
+  
+  target <- site %>% 
+    html_nodes(xpath="//div//p//em") %>%
+    extract2(1) %>%
+    html_text()
+  target <- unlist(strsplit(target, "\\s+"))[4]
+  
+  perc_raised <- site %>% 
+    html_nodes(xpath="//div//strong//em") %>%
+    extract2(1) %>%
+    html_text()
+  
+  sum_raised <- site %>%
+    html_nodes(xpath="//div//p//span[@class='raised-so-far']") %>%
+    html_text()
+  
+  n_donations <- site %>%
+    html_nodes(xpath="//div//p//span[@class='number-of-donations']") %>%
+    html_text() %>%
+    as.numeric()
+  
+  dat <- data.frame(perc_raised=perc_raised, )
+  
+  return(target=target, )
+}
